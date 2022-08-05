0c7243694316195913dde14fbcac71d13fa5ac57
benjamin efosa <benjaminefosa@yahoo.com>
2022-08-05 11:32:10 +0100

    router update

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react'
-// make sure to use https
 import useFetch from './useFetch'
+
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
 const AppContext = React.createContext()
@@ -16,7 +16,6 @@ const AppProvider = ({ children }) => {
   )
 }
 
-// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
